b9d3af67a7a79d9f429a83e832d631fc1fc4cf5a
Ned Zimmerman <ned@bight.dev>
2021-01-20 17:27:49 -0400

    feat: pagination and filtering

--- src/assets/scripts/Activities.svelte
+++ src/assets/scripts/Activities.svelte
@@ -1,12 +1,17 @@
 <script>
     import { onMount } from 'svelte';
 
-    import { url, activities, pageCount, type, experience, subject } from './store.module.js'
+    import { url, activities, filteredActivities, pages, pageCount, type, experience, subject } from './store.module.js'
 
     let offset = 0;
-    let pages = [];
     let params = new URLSearchParams(window.location.search);
     let page = parseInt(params.get('page')) || 1;
+
+    let selectedType = params.get('type') || "";
+    let selectedExperience = params.get('experience') || "";
+    let selectedSubject = params.get('subject') || "";
+
+    let filtered = false;
 
     let types = [
         "Unplugged",
@@ -17,12 +22,54 @@
     let levels = require("../../_data/activity-levels.json");
     let subjects = require("../../_data/activity-subjects.json");
 
+    /*!
+    * Build a query string from an object of data
+    * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+    * @param  {Object} data The data to turn into a query string
+    * @return {String}      The query string
+    */
+    var buildQuery = function (data) {
+        if (typeof (data) === 'string') return data;
+        var query = [];
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+            }
+        }
+        return query.join('&');
+    };
+
     function applyFilters() {
-        console.log({$type, $experience, $subject});
+        let queryParams = {};
+
+        if (selectedType) {
+            queryParams.type = selectedType.toLowerCase();
+        }
+
+        if (selectedExperience) {
+            queryParams.experience = selectedExperience.toLowerCase();
+        }
+
+        if (selectedSubject) {
+            queryParams.subject = selectedSubject.toLowerCase();
+        }
+
+        queryParams.page = 1;
+
+        const href = `?${buildQuery(queryParams)}`;
+
+        $type = selectedType;
+        $experience = selectedExperience;
+        $subject = selectedSubject;
+
+        history.pushState(href, '', href);
+        
+        page = 1;
+        offset = 0;
     }
 
     function clearFilters() {
-        $type = $experience = $subject = '';
+        selectedType = selectedExperience = selectedSubject = '';
         applyFilters();
     }
 
@@ -38,9 +85,7 @@
 		const res = await fetch(`/activities.json`);
         $activities = await res.json();
         offset = (page - 1) * 10;
-        for(let i = 1; i <= $pageCount; i++) {
-            pages.push((i - 1) * 10);
-        }
+        applyFilters();
 	});
 </script>
 
@@ -49,7 +94,7 @@
     <div class="space-y-4 lg:space-y-0 lg:flex lg:justify-start lg:items-center">
         <label class="block lg:flex lg:items-center" for="type">
             Type: 
-            <select bind:value={$type} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="type" id="type">
+            <select bind:value={selectedType} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="type" id="type">
                 <option value="">Any Type</option>
                 {#each types as t}
 				<option value={t}>{t}</option>
@@ -58,7 +103,7 @@
         </label>
         <label class="block lg:flex lg:items-center lg:ml-4" for="experience">
             Experience:        
-            <select bind:value={$experience} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="experience" id="experience">
+            <select bind:value={selectedExperience} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="experience" id="experience">
                 <option value="">Any Level</option>
                 {#each levels as l}
 				<option value={l}>{l}</option>
@@ -67,7 +112,7 @@
         </label>
         <label class="block lg:flex lg:items-center lg:ml-4" for="subject">
             Subject:
-            <select bind:value={$subject} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="subject" id="subject">
+            <select bind:value={selectedSubject} class="py-1 ml-2 border border-black rounded focus:outline-none focus:ring-2" name="subject" id="subject">
                 <option value="">Any Subject</option>
                 {#each subjects as s}
 				<option value={s}>{s}</option>
@@ -77,16 +122,14 @@
         <button on:click={applyFilters} class="px-2 py-1 font-semibold border border-black rounded lg:ml-4 focus:outline-none focus:ring-2" id="apply" type="button">Apply Filters</button>
         <button on:click={clearFilters} class="px-2 py-1 border border-black rounded lg:ml-auto focus:outline-none focus:ring-2" id="clear" type="button">Clear Filters</button>
     </div>
+    {#if $filteredActivities.length < $activities.length}
+    <p>Showing {$filteredActivities.length} of {$activities.length} activities.</p>
+    {/if}
 </div>
-
-<p>Current page: {page}</p>
-<p>Total items: {$activities.length}</p>
-<p>Total pages: {$pageCount}</p>
-
 <div class="grid gap-6 my-12 activities md:grid-cols-2">
     <h2 class="sr-only">Activities</h2>
-    {#if $activities.length > 0}
-        {#each $activities.slice(offset, offset + 10) as item}
+    {#if $filteredActivities.length > 0}
+        {#each $filteredActivities.slice(offset, offset + 10) as item}
             <article class="relative border border-black card entry focus-within:ring-2">
                 <svg viewBox="0 0 570 393" class="w-auto max-w-full"><rect width="570" height="393" class="fill-current"></rect></svg>
                 <div class="p-4 space-y-4">
@@ -100,17 +143,17 @@
         {/each}
     {/if}
 </div>
-{#if $activities.length > 10}
+{#if $filteredActivities.length > 10}
 <nav class="w-full mb-12 pagination" aria-label="pagination">
     <ul class="flex flex-row items-center justify-center w-full">
     {#if offset > 0}
-        <li class="ml-2"><a on:click|preventDefault={changePage} class="p-2" href="?page={page - 1}" data-page={page - 1}><span aria-hidden="true">&larr;</span><span class="sr-only">previous</span></a></li>
+        <li class="ml-2"><a on:click|preventDefault={changePage} class="p-2" href={window.location.search.replace(/page=([0-9])*/g, `page=${page - 1}`)} data-page={page - 1}><span aria-hidden="true">&larr;</span><span class="sr-only">previous</span></a></li>
     {/if}
-    {#each pages as p, i}
-    <li class="ml-2"><a on:click|preventDefault={changePage} class="{offset === p ? "p-2 text-white bg-black" : "p-2"}" href="?page={i + 1}" data-page={i + 1}>{i + 1}</a></li>
+    {#each $pages as p, i}
+    <li class="ml-2"><a on:click|preventDefault={changePage} class="{offset === p ? "p-2 text-white bg-black" : "p-2"}" href={window.location.search.replace(/page=([0-9])*/g, `page=${i + 1}`)} data-page={i + 1}>{i + 1}</a></li>
     {/each}
-    {#if $activities.length > offset + 10}
-        <li class="ml-2"><a on:click|preventDefault={changePage} class="p-2" href="?page={page + 1}" data-page={page + 1}><span class="sr-only">next</span><span aria-hidden="true">&rarr;</span></a></li>
+    {#if $filteredActivities.length > offset + 10}
+        <li class="ml-2"><a on:click|preventDefault={changePage} class="p-2" href={window.location.search.replace(/page=([0-9])*/g, `page=${page + 1}`)} data-page={page + 1}><span class="sr-only">next</span><span aria-hidden="true">&rarr;</span></a></li>
     {/if}
     </ul>
 </nav>
--- src/assets/scripts/store.module.js
+++ src/assets/scripts/store.module.js
@@ -30,8 +30,26 @@ const activities = writable([]);
 const type = writable("");
 const experience = writable("");
 const subject = writable("");
-const pageCount = derived(activities, $activities => Math.ceil($activities.length / 10));
+const filteredActivities = derived([activities, type, experience, subject], ([$activities, $type, $experience, $subject]) => {
+    return $activities.filter(activity => {
+        if (($type === "" || activity.type === $type) && ($experience === "" || activity.experience === $experience) && ($subject === "" || activity.subject === $subject)) {
+            return true;
+        }
+
+        return false;
+    });
+});
+const pageCount = derived(filteredActivities, $filteredActivities => Math.ceil($filteredActivities.length / 10));
+const pages = derived(pageCount, $pageCount => {
+    let pages = [];
+
+    for (let i = 1; i <= $pageCount; i++) {
+        pages.push((i - 1) * 10);
+    }
+
+    return pages;
+});
 
 export {
-    url, activities, pageCount, type, experience, subject
+    url, activities, filteredActivities, pageCount, pages, type, experience, subject
 };
