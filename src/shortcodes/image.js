cc410fc4633cf8ee65f22eecdbf05a57bfe151de
Ned Zimmerman <ned@bight.dev>
2021-04-20 09:21:41 -0300

    fix: simplify image shortcode

--- src/shortcodes/image.js
+++ src/shortcodes/image.js
@@ -1,21 +1,23 @@
 "use strict";
 
-const eleventyImage = require("@11ty/eleventy-img");
+// const eleventyImage = require("@11ty/eleventy-img");
 
-module.exports = async (src, alt, sizes) => {
-    const metadata = await eleventyImage(src, {
-        outputDir: "./dist/assets/media/",
-        urlPath: "/assets/media/",
-        widths: [320, 640],
-        formats: ["avif", "jpeg"]
-    });
+module.exports = async (src, alt) => {
+    // const metadata = await eleventyImage(src, {
+    //     outputDir: "./dist/assets/media/",
+    //     urlPath: "/assets/media/",
+    //     widths: [320, 640],
+    //     formats: ["avif", "jpeg"]
+    // });
 
-    const imageAttributes = {
-        alt,
-        sizes,
-        loading: "lazy",
-        decoding: "async"
-    };
+    // const imageAttributes = {
+    //     alt,
+    //     sizes,
+    //     loading: "lazy",
+    //     decoding: "async"
+    // };
 
-    return eleventyImage.generateHTML(metadata, imageAttributes);
+    // return eleventyImage.generateHTML(metadata, imageAttributes);
+
+    return `<picture><img src="${src}" alt="${alt}" /></picture>`;
 };
