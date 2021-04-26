5402890a2508a91fe4ad3dcc90904858d2cfd2b7
Ned Zimmerman <ned@bight.dev>
2021-04-26 09:01:14 -0300

    feat: implement navigation (resolves #329) (#344)
    * feat: add postcss-logical
    
    * feat: implement navigation (resolves #329)
    
    * feat: use mask-image with fallback
    
    * feat: implement new navigation design

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -13,14 +13,17 @@ https://github.com/fluid-project/fluidic-11ty/raw/master/LICENSE.md.
 import "@zachleat/filter-container";
 
 document.addEventListener("click", event => {
-    if (event.target.id !== "reset") {
-        return;
+    if (event.target.id === "reset") {
+        const filters = [...event.target.parentNode.querySelectorAll("select")];
+
+        filters.forEach(filter => {
+            filter.value = "";
+            filter.dispatchEvent(new Event("change", {"bubbles": true}));
+        });
     }
 
-    const filters = [...event.target.parentNode.querySelectorAll("select")];
-
-    filters.forEach(filter => {
-        filter.value = "";
-        filter.dispatchEvent(new Event("change", {"bubbles": true}));
-    });
+    // if (event.target.id === "menu-toggle") {
+    //     const expanded = event.target.getAttribute("aria-expanded") === "true";
+    //     event.target.setAttribute("aria-expanded", !expanded);
+    // }
 });
