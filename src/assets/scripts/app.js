5aea0654cdf02d89945f1244924a29ad72a801b7
Ned Zimmerman <ned@bight.dev>
2022-06-21 15:28:08 -0300

    fix: restore filter functionality (#712)

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -15,16 +15,22 @@ import "wicg-inert";
 
 import VideoOverlay from "./overlay.module.js";
 
-document.addEventListener("click", event => {
-    if (event.target.id === "reset") {
-        const filters = [...event.target.parentNode.querySelectorAll("select")];
+// document.addEventListener("click", event => {
+//     if (event.target.id === "reset") {
+//         const filters = [...event.target.parentNode.querySelectorAll("select")];
 
-        filters.forEach(filter => {
-            filter.value = "";
-            filter.dispatchEvent(new Event("change", {"bubbles": true}));
-        });
-    }
-});
+//         filters.forEach(filter => {
+//             filter.addEventListener("change", function () {
+//                 console.log("changed");
+//             });
+
+//             filter.value = "";
+//             filter.dispatchEvent(new Event("change", {"bubbles": true}));
+//         });
+
+
+//     }
+// });
 
 const linkedVideos = [...document.querySelectorAll(".linked-video")];
 
